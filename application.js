4fa347474880e3c9d448a003c140b0d8c086059b
Chris Jackson <cjackson290@email.itt-tech.edu>
2015-06-29 21:47:20 -0500

    index.html2

---
+++ application.js
@@ -0,0 +1,24 @@
+// Define Global
+var toDoList = [];
+
+function changeDisplay(){
+	var list = document.getElementById("todolist");
+	//only place we actually change the screen
+	list.innerHTML = "";
+	for(var i=0; i<toDoList.length; i++) {
+		list.innerHTML += '<div class="item">' + toDoList[i] + '</div>';
+	}
+}
+
+function addToDo() {
+	var tmpItem;
+	var newToDo = document.getElementById("todonew");
+	tmpItem = newToDo.value;
+	
+	toDoList.push(tmpItem);
+	// Call our display function
+	changeDisplay();
+	
+	newToDo.value= "";
+	newToDo.select();
+}
