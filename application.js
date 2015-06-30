0812a2a039a2ccebb58474f5f8065bf6e26f8829
Chris Jackson <cjackson290@email.itt-tech.edu>
2015-06-29 22:31:30 -0500

    Name(X)

--- application.js
+++ application.js
@@ -6,10 +6,22 @@ function changeDisplay(){
 	//only place we actually change the screen
 	list.innerHTML = "";
 	for(var i=0; i<toDoList.length; i++) {
-		list.innerHTML += '<div class="item">' + toDoList[i] + '</div>';
+		list.innerHTML += '<div class="item">'
+		+ toDoList[i]
+		+ ' (<a href="#" onclick="javascript:removeToDo(' + i +')">X</a>)</div>';
 	}
 }
-
+function removeToDo(itemToRemove){
+	var newList =[];
+	for(var i=0;i<toDoList.length;i++){
+		if( i !== itemToRemove) {
+			newList.push(toDoList[i]);
+		}
+	}
+	toDoList=newList;
+	changeDisplay();
+}
+	
 function addToDo() {
 	var tmpItem;
 	var newToDo = document.getElementById("todonew");
